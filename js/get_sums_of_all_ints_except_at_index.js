d2fc0391050c09b213651a08edf57a8a44152442
Marcode777 <marc102202@hotmail.com>
2016-01-16 10:37:00 -0500

    made our pseudo code, it's the theory and main idea behind what we'll do

---
+++ js/get_sums_of_all_ints_except_at_index.js
@@ -0,0 +1,7 @@
+// the integers 1, 7, 3, and 4 will be added into a sum
+// it will skip placeholder to the next one from left to right
+//for the first result, 14, we will add placeholders 1, 2, 3 from the array
+// the the second result, 8, we will add placeholders, 0, 2 and 3 from the array
+// for the third result, 12, we will add placeholders 0, 1, and 3 from the array
+// for the fourth result, 11, we will add placeholders, 0, 1, 2 from the array
+
